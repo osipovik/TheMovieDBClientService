2be79cf5af21f8da11a767096623595de9e6acb4
osipov <osipovik89@gmail.com>
2017-11-29 23:24:00 +0300

    Initial commit

---
+++ src/main/java/com/loyaltyplant/movie_info/MovieInfoMain.java
@@ -0,0 +1,12 @@
+package com.loyaltyplant.movie_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieInfoMain.class, args);
+    }
+}
